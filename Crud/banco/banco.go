69e5da7c672c962299fd4740f3d4871e8096b36e
declau <dec82@hotmail.com>
2020-12-17 16:27:40 -0300

    crud

---
+++ Crud/banco/banco.go
@@ -0,0 +1,26 @@
+package banco
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql" //Driver de conexão com o MySQL
+)
+
+//Conectar abre a conexão com o DB
+func Conectar() (*sql.DB, error) {
+	stringConexao := "golang:golang@/devbook?charset=utf8&parseTime=true&loc=Local"
+
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		return nil, erro
+	}
+
+	fmt.Println("Conexão Aberta!")
+
+	return db, nil
+}
---
+++ Crud/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	//CRUD -> Create, Read, Update, Delete
+
+	router := mux.NewRouter()
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
